c3e4812455bcf3b7c8cd52339c8825214f1936e4
Manu <44340862+ManuelDC-web@users.noreply.github.com>
2024-01-22 23:40:36 +0100

    bbdd actualizada

---
+++ bd_foro (2).sql
@@ -0,0 +1,159 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 22-01-2024 a las 23:40:02
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `bd_foro`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `mensajes`
+--
+
+CREATE TABLE `mensajes` (
+  `men_id` int(11) NOT NULL,
+  `men_mensaje` mediumtext NOT NULL,
+  `men_tema_id` int(11) NOT NULL,
+  `men_usu_id` int(11) NOT NULL,
+  `men_fecha_hora` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `mensajes`
+--
+
+INSERT INTO `mensajes` (`men_id`, `men_mensaje`, `men_tema_id`, `men_usu_id`, `men_fecha_hora`) VALUES
+(114, '    ¿Qué tengo mal aquí?', 53, 2, '2024-01-22 21:37:51'),
+(116, '    ¿Cuál es la mejor versión?', 53, 33, '2024-01-22 21:39:40'),
+(119, '    ORIGEN DEL JS', 54, 32, '2024-01-22 21:42:44'),
+(121, '    ¿Qué hago?', 54, 1, '2024-01-22 21:47:42'),
+(123, '    HELP ME!', 16, 2, '2024-01-22 21:49:14'),
+(124, '    ¿Soluciones?', 52, 32, '2024-01-22 21:50:33'),
+(126, '    ¿Me ayudáis?', 52, 1, '2024-01-22 21:51:44');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `temas`
+--
+
+CREATE TABLE `temas` (
+  `tema_id` int(11) NOT NULL,
+  `tema_tema` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `temas`
+--
+
+INSERT INTO `temas` (`tema_id`, `tema_tema`) VALUES
+(16, 'MongoDB'),
+(52, 'PHP'),
+(53, 'JAVA'),
+(54, 'JS');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `usu_id` int(11) NOT NULL,
+  `usu_nombre` varchar(50) NOT NULL,
+  `usu_alias` varchar(50) NOT NULL,
+  `usu_password` varchar(256) NOT NULL,
+  `usu_foto` varchar(50) NOT NULL,
+  `usu_admin` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`usu_id`, `usu_nombre`, `usu_alias`, `usu_password`, `usu_foto`, `usu_admin`) VALUES
+(1, 'administrador', 'administrador', '91f5167c34c400758115c2a6826ec2e3', 'u01.gif', 1),
+(2, 'usuario', 'usuario', 'f8032d5cae3de20fcec887f395ec9a6a', 'u02.gif', 0),
+(32, 'manuel', 'manuel', '96917805fd060e3766a9a1b834639d35', 'u01.gif', 1),
+(33, 'alex', 'alex', '534b44a19bf18d20b71ecc4eb77c572f', 'u01.gif', 1),
+(39, 'Javier', 'Javier', 'ab02fceb9689114b1cd1844e456c0695', 'u02.gif', 0);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `mensajes`
+--
+ALTER TABLE `mensajes`
+  ADD PRIMARY KEY (`men_id`),
+  ADD KEY `men_tema_id` (`men_tema_id`),
+  ADD KEY `men_usu_id` (`men_usu_id`);
+
+--
+-- Indices de la tabla `temas`
+--
+ALTER TABLE `temas`
+  ADD PRIMARY KEY (`tema_id`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`usu_id`),
+  ADD UNIQUE KEY `usu_alias` (`usu_alias`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `mensajes`
+--
+ALTER TABLE `mensajes`
+  MODIFY `men_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=127;
+
+--
+-- AUTO_INCREMENT de la tabla `temas`
+--
+ALTER TABLE `temas`
+  MODIFY `tema_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=56;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `usu_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `mensajes`
+--
+ALTER TABLE `mensajes`
+  ADD CONSTRAINT `mensajes_ibfk_1` FOREIGN KEY (`men_usu_id`) REFERENCES `usuarios` (`usu_id`),
+  ADD CONSTRAINT `mensajes_ibfk_2` FOREIGN KEY (`men_tema_id`) REFERENCES `temas` (`tema_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
